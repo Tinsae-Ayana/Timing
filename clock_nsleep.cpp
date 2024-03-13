52780d44c79f30de48fcc463e6438838942a5c23
Tinsae-Ayana <tinsaeaeroastro@gmail.com>
2024-03-13 15:04:10 +0100

    initial

---
+++ clock_nsleep.cpp
@@ -0,0 +1,124 @@
+// this is for  nano sleep method
+#include <iostream>
+#include <pthread.h>
+#include <time.h>
+
+#define INTERVAL_NS 1000000 // 1.0 ms in nanoseconds
+using namespace std;
+
+void some_random_computation() {
+  // calculate the sum from 1 to 10000
+  long long sum = 0;
+  for (long long i = 100000; i >= 0; i--) {
+    sum = sum + i;
+  }
+}
+
+void *periodic_task(void *threadinput) {
+
+  struct timespec currentStartTime, nextTime, lastStartTime, timeBeforeSleep,
+      interval;
+  bool isFirstIteration = true;
+  interval.tv_sec = 0;
+  interval.tv_nsec = INTERVAL_NS;
+
+  // create a file to store the execution time (time taken by the math stuff)
+  FILE *file = fopen("data2//execution_time2.txt", "a");
+  if (file == NULL) {
+    perror("Error opening file");
+    exit(EXIT_FAILURE);
+  }
+
+  // creat a file to store the idle time (time the process sleeps)
+  FILE *file2 = fopen("data2//idle_time2.txt", "a");
+  if (file == NULL) {
+    perror("Error opening file");
+    exit(EXIT_FAILURE);
+  }
+
+  // create a file to store the sample time (time between conscutive loops)
+  FILE *file3 = fopen("data2//sample_time2.txt", "a");
+  if (file == NULL) {
+    perror("Error opening file");
+    exit(EXIT_FAILURE);
+  }
+
+  // array to store timing information, this is to avoid writing to file inside
+  // the loop as per the assignment instruction
+  long long int execution_time[100];
+  long long int idle_time[100];
+  long long int sample_time[100];
+
+  for (int i = 0; i < 100; i++) {
+    // record the current time at the beginning of each iteration
+    clock_gettime(CLOCK_MONOTONIC, &currentStartTime);
+
+    if (!isFirstIteration) {
+      // calculate the time interval from the start of previous iteration to
+      // current iteration
+      long sample_time_ns =
+          (currentStartTime.tv_sec - lastStartTime.tv_sec) * 1000000000L +
+          (currentStartTime.tv_nsec - lastStartTime.tv_nsec);
+      sample_time[i] = sample_time_ns;
+
+      // calculate the idle time
+      long long idle_time_ns =
+          (currentStartTime.tv_sec - timeBeforeSleep.tv_sec) * 1000000000 +
+          (currentStartTime.tv_nsec - timeBeforeSleep.tv_nsec);
+      idle_time[i] = idle_time_ns;
+
+    } else {
+      isFirstIteration = false;
+    }
+
+    // save the start of current iteration to use it as last start in next
+    // iteration
+    lastStartTime = currentStartTime;
+
+    // update next wait time. TODO:
+    nextTime.tv_sec += (nextTime.tv_nsec + interval.tv_nsec) / 1000000000;
+    nextTime.tv_nsec = (nextTime.tv_nsec + interval.tv_nsec) % 1000000000;
+
+    // do some computation
+    some_random_computation();
+
+    // get absolute time before sleep
+    clock_gettime(CLOCK_MONOTONIC, &timeBeforeSleep);
+
+    // calculate and store the time taken by math computation to be executed
+    long long execution_time_ns =
+        (timeBeforeSleep.tv_sec - currentStartTime.tv_sec) * 1000000000 +
+        (timeBeforeSleep.tv_nsec - currentStartTime.tv_nsec);
+    execution_time[i] = execution_time_ns;
+
+    // sleep the remaining idle time
+    clock_nanosleep(CLOCK_MONOTONIC, TIMER_ABSTIME, &nextTime, NULL);
+  }
+
+  // save the data to file
+  for (int i = 0; i < 100; i++) {
+    fprintf(file, "%lld\n", execution_time[i]);
+  }
+
+  for (int i = 0; i < 100; i++) {
+    fprintf(file2, "%lld\n", idle_time[i]);
+  }
+
+  for (int i = 0; i < 100; i++) {
+    fprintf(file3, "%lld\n", sample_time[i]);
+  }
+
+  // close the files
+  fclose(file);
+  fclose(file2);
+  fclose(file3);
+
+  return NULL;
+}
+
+int main() {
+  pthread_t periodic;
+  pthread_create(&periodic, nullptr, periodic_task, nullptr);
+  pthread_join(periodic, nullptr);
+  return 0;
+}
---
+++ signal_nwait.cpp
@@ -0,0 +1,148 @@
+#include <iostream>
+#include <signal.h>
+#include <stdio.h>
+#include <time.h>
+
+#define TIME_INTERVAL_NANO 1000000
+
+sigset_t start_periodic_timer() {
+
+  // create timer
+  timer_t timerid;
+  struct sigevent sev;
+  sev.sigev_notify = SIGEV_SIGNAL;
+  sev.sigev_signo = SIGALRM;
+  timer_create(CLOCK_MONOTONIC, &sev, &timerid);
+
+  // set timerframe for 1ms
+  struct itimerspec its;
+  its.it_value.tv_sec = 0;
+  its.it_value.tv_nsec = 1000000;
+  its.it_interval.tv_sec = 0;
+  its.it_interval.tv_nsec = 1000000;
+  timer_settime(timerid, 0, &its, nullptr); // set/start timer
+
+  // creating signal set that sigwait needs to wait for
+  sigset_t signalset;
+  sigemptyset(&signalset);
+  sigaddset(&signalset, SIGALRM);
+
+  // block SIGALRM before calling sigwait because it could be handled by default
+  // handles asynchroneously and signwait wont' catch it, causes the program to
+  // terminate
+
+  sigprocmask(SIG_BLOCK, &signalset, NULL);
+
+  return signalset;
+}
+
+void some_random_computation() {
+  // calculate the sum from 1 to 10000
+  // std::cout << "doing random computation" << std::endl;
+  long long int sum = 0;
+  for (long long int i = 1000; i >= 0; i--) {
+    sum = sum + i;
+  }
+}
+
+void *periodic_task(void *threadinput) {
+
+  struct timespec start_time, before_sleep_time, after_sleep_time;
+
+  // create a file to store the execution time
+  FILE *file = fopen("data//execution_time2.txt", "a");
+  if (file == NULL) {
+    perror("Error opening file");
+    exit(EXIT_FAILURE);
+  }
+
+  // creat a file to store the idle time
+  FILE *file2 = fopen("data//idle_time2.txt", "a");
+  if (file == NULL) {
+    perror("Error opening file");
+    exit(EXIT_FAILURE);
+  }
+
+  // creat a file to store the idle time
+  FILE *file3 = fopen("data//sample_time2.txt", "a");
+  if (file == NULL) {
+    perror("Error opening file");
+    exit(EXIT_FAILURE);
+  }
+
+  // array to store timing information, this is to avoid writing to file inside
+  // the loop as per the assignment instruction
+  long long int execution_time[100];
+  long long int idle_time[100];
+  long long int sample_time[100];
+
+  // start the timer
+  sigset_t signalset = start_periodic_timer();
+
+  for (int index = 0; index < 100; index++) {
+
+    // get time before the math operation starts
+    clock_gettime(CLOCK_MONOTONIC, &start_time);
+    some_random_computation();
+    clock_gettime(CLOCK_MONOTONIC, &before_sleep_time);
+
+    // calculate and store the time taken by the math computation to be executed
+    long long execution_time_ns =
+        (before_sleep_time.tv_sec - start_time.tv_sec) * 1000000000 +
+        (before_sleep_time.tv_nsec - start_time.tv_nsec);
+    execution_time[index] = execution_time_ns;
+
+    // sleep until signal is generated
+    int sig;
+    sigwait(&signalset, &sig);
+
+    // calculate the sample time
+    clock_gettime(CLOCK_MONOTONIC, &after_sleep_time);
+    long long sample_time_ns =
+        (after_sleep_time.tv_sec - start_time.tv_sec) * 1000000000 +
+        (after_sleep_time.tv_nsec - start_time.tv_nsec);
+    sample_time[index] = sample_time_ns;
+
+    // calculate the idle time
+    long long idle_time_ns =
+        (after_sleep_time.tv_sec - before_sleep_time.tv_sec) * 1000000000 +
+        (after_sleep_time.tv_nsec - before_sleep_time.tv_nsec);
+    idle_time[index] = idle_time_ns;
+  }
+
+  // save the data to file
+  for (int i = 0; i < 100; i++) {
+    fprintf(file, "%lld\n", execution_time[i]);
+  }
+
+  for (int i = 0; i < 100; i++) {
+    fprintf(file2, "%lld\n", idle_time[i]);
+  }
+
+  for (int i = 0; i < 100; i++) {
+    fprintf(file3, "%lld\n", sample_time[i]);
+  }
+
+  // close the files
+  fclose(file);
+  fclose(file2);
+  fclose(file3);
+  return nullptr;
+}
+
+int main() {
+
+  // creating signal set that sigwait needs to wait for on the other thread but
+  // in the main thread, it needs to blocked before it causes the program to
+  // terminate
+  sigset_t signalset;
+  sigemptyset(&signalset);
+  sigaddset(&signalset, SIGALRM);
+  sigprocmask(SIG_BLOCK, &signalset, NULL);
+
+  // spawn a thread for the period task
+  pthread_t thread;
+  pthread_create(&thread, nullptr, periodic_task, nullptr);
+  pthread_join(thread, nullptr);
+  return 0;
+}
